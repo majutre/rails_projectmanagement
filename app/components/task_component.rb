e5e0989a1cffbfa60c7119a6bca0b5d9590dfe46
Maju <majutrevisan@gmail.com>
2021-09-15 14:49:28 -0300

    Add task component

---
+++ app/components/task_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TaskComponent < ViewComponent::Base
+    attr_reader :task
+
+    def initialize(task:)
+        @task = task
+    end
+
+    def render?
+        task.persisted?
+    end
+
+end
